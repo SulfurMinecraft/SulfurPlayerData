3b6d36f978e5a76e27529ad3d7723ed75d4c479e
jgj52 <email@jgj52.hu>
2026-04-09 16:01:42 +0200

    1.2

---
+++ src/main/java/hu/jgj52/SulfurPlayerData/SaverListener.java
@@ -0,0 +1,27 @@
+package hu.jgj52.SulfurPlayerData;
+
+import com.quietterminal.cattlelog.PlayerDataDeserializer;
+import com.quietterminal.cattlelog.PlayerDataSerializer;
+import hu.jgj52.Sulfur.Utils.Listeners.Event;
+import hu.jgj52.Sulfur.Utils.Listeners.Listener;
+import net.minestom.server.event.player.PlayerDisconnectEvent;
+import net.minestom.server.event.player.PlayerSpawnEvent;
+
+public class SaverListener extends Listener {
+    @Event(priority = 10)
+    public void onJoin(PlayerSpawnEvent event) {
+        if (!event.isFirstSpawn()) return;
+
+        SulfurPlayerData.fileManager.load(event.getPlayer().getUuid()).ifPresent(cow ->
+            PlayerDataDeserializer.deserialize(event.getPlayer(), cow)
+        );
+    }
+
+    @Event
+    public void onLeave(PlayerDisconnectEvent event) {
+        SulfurPlayerData.fileManager.save(
+                event.getPlayer().getUuid(),
+                PlayerDataSerializer.serialize(event.getPlayer())
+        );
+    }
+}
--- src/main/java/hu/jgj52/SulfurPlayerData/SulfurPlayerData.java
+++ src/main/java/hu/jgj52/SulfurPlayerData/SulfurPlayerData.java
@@ -1,14 +1,35 @@
 package hu.jgj52.SulfurPlayerData;
 
-import com.quietterminal.cattlelog.Cattlelog;
+import com.quietterminal.cattlelog.CowFileManager;
+import com.quietterminal.cattlelog.PlayerDataSerializer;
 import hu.jgj52.Sulfur.Utils.Plugin;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
 
 import java.nio.file.Path;
 
 public class SulfurPlayerData implements Plugin {
+    public static CowFileManager fileManager;
     @Override
     public void onEnable() {
-        Cattlelog.initialize(Path.of("playerdata"));
+        fileManager = new CowFileManager(Path.of("playerdata"));
+
+        try {
+            fileManager.ensureBarnExists();
+        } catch (java.io.IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        new SaverListener();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            for (Player player : MinecraftServer.getConnectionManager().getOnlinePlayers()) {
+                fileManager.save(
+                        player.getUuid(),
+                        PlayerDataSerializer.serialize(player)
+                );
+            }
+        }));
     }
 
     @Override
